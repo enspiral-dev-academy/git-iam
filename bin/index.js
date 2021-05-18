856fdb95d1010693e4195b3e2f93d2734d7839a6
Lache Melvin <lache.melvin@gmail.com>
2021-05-18 13:52:33 +1200

    add --build option for creating users.json

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,20 @@
 #! /usr/bin/env node
 
+const fs = require('fs').promises
+const path = require('path')
 const shell = require('shelljs')
 const request = require('superagent')
+const { Octokit } = require('@octokit/rest')
 
 const alias = process.argv[2].toLowerCase()
-const listingUrl = process.argv[3]
+const inputPath = process.argv[3]
 
 if (!alias) {
   showUsage()
 } else if (alias === '--init') {
-  setupExtension(listingUrl)
+  setupExtension(inputPath)
+} else if (alias === '--build') {
+  createUsersJson(inputPath)
 } else {
   configureUser(alias)
 }
@@ -61,3 +66,74 @@ async function getUserListing (url) {
   }
 }
 
+async function createUsersJson (inputPath) {
+  const developers = await getDevData(inputPath)
+  const userData = await getUserData(developers)
+
+  try {
+    const formattedUserData = userData.map((result, idx) => {
+      const { id, login, name } = result.data
+      return {
+        name: name || login,
+        email: id + '+' + login + '@users.noreply.github.com',
+        alias: Object.keys(developers)[idx]
+      }
+    })
+      .sort((user1, user2) => {
+        if (user1.alias < user2.alias) {
+          return -1
+        }
+        if (user1.alias > user2.alias) {
+          return 1
+        }
+        return 0
+      })
+      .reduce((userObj, user) => {
+        const { alias, name, email } = user
+        userObj[alias] = { name, email }
+        return userObj
+      }, {})
+
+    const userJSON = JSON.stringify(formattedUserData, null, 2)
+    console.info(`\nSuccess! Here's your git iam JSON object. Save it to a Gist!`)
+    console.info(userJSON)
+  } catch (err) {
+    console.error('An error occurred while trying to create your users JSON object:')
+    console.error(err)
+    showUsage()
+  }
+}
+
+async function getDevData (devsPath) {
+  try {
+    const devData = await fs.readFile(path.resolve(devsPath))
+    return JSON.parse(devData)
+  } catch (err) {
+    console.error('An error occurred while trying to read your input JSON file:')
+    console.error(err)
+    showUsage()
+    process.exit(1)
+  }
+}
+
+async function getUserData (developers) {
+  const authToken = process.env.GH_AUTH
+
+  if (!authToken) {
+    console.error('Could not find your GitHub Personal Access Token in GH_AUTH')
+    console.info('You can proceed after you complete that step.')
+    process.exit(1)
+  }
+
+  const github = new Octokit({ auth: authToken })
+
+  try {
+    const results = await Promise.all(Object.values(developers).map(username => github.users.getByUsername({ username })))
+    return results
+  } catch (err) {
+    console.error('An error occurred while trying to access the user data from GitHub:')
+    console.error(err)
+    showUsage()
+    process.exit(1)
+  }
+}
